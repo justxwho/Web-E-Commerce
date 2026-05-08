37285fcf5d62200e4264e5e8945458eae51149dd
Ha Nhat Nam <hanhatnam0912@gmail.com>
2026-05-09 00:39:25 +0700

    Fix change icon heart color in shop page

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -19,18 +19,6 @@
         input[type="checkbox"] {
             accent-color: #222222;
         }
-
-        .heart-active {
-            fill: red !important;
-        }
-
-        .heart-active use {
-            fill: red !important;
-        }
-
-        .pc__btn-wl {
-            fill: red !important;
-        }
     </style>
     <main class="pt-90">
         <section class="shop-main container d-flex pt-4 pt-xl-5">
@@ -508,15 +496,30 @@
                                         <input type="hidden" name="price"
                                             value="{{ $product->sale_price == '' ? $product->regular_price : $product->sale_price }}">
                                         <input type="hidden" name="quantity" value="1">
-                                        <button type="submit"
-                                            class="pc__btn-wl position-absolute top-0 end-0 bg-transparent border-0 js-add-wishlist"
-                                            title="Add To Wishlist">
-                                            <svg width="16" height="16" viewBox="0 0 20 20"
-                                                class="{{ $inWishlist ? 'heart-active' : '' }}"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <use href="#icon_heart" />
-                                            </svg>
-                                        </button>
+                                        @if (!$inWishlist)
+                                            <button type="submit"
+                                                class="pc__btn-wl position-absolute top-0 end-0 bg-transparent border-0 js-add-wishlist"
+                                                title="Add To Wishlist">
+                                                <svg width="16" height="16" viewBox="0 0 20 20"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <use href="#icon_heart" />
+                                                </svg>
+                                            </button>
+                                        @else
+                                            <button title="Add To Wishlist"
+                                                class="pc__btn-wl position-absolute top-0 end-0 bg-transparent border-0 js-add-wishlist">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                    viewBox="0 0 24 24" fill="#e74c3c">
+
+                                                    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5
+                                                                2 5.42 4.42 3 7.5 3
+                                                                c1.74 0 3.41.81 4.5 2.09
+                                                                C13.09 3.81 14.76 3 16.5 3
+                                                                19.58 3 22 5.42 22 8.5
+                                                                c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+                                                </svg>
+                                            </button>
+                                        @endif
                                     </form>
                                 </div>
                             </div>
