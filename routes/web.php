ffcbaa597e4331416ace352d99dbe37392374f22
Ha Nhat Nam <hanhatnam0912@gmail.com>
2026-05-18 17:07:51 +0700

    User Show Order and Order Details

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,8 @@ Route::prefix('checkout')->group(function () {
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+    Route::get('/account-orders', [UserController::class, 'orders'])->name('user.orders');
+    Route::get('/account-order/{order_id}/details', [UserController::class, 'order_details'])->name('user.order.details');
 });
 
 Route::middleware(['auth', AuthAdmin::class])->group(function () {
